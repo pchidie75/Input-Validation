9f00e986ecbf789413e9b2bc79720d2a52d42792
pchidie75 <pchidie75@users.noreply.github.com>
2020-11-17 21:24:24 -0500

    Add files via upload

---
+++ SortedStudentsApp_CO/src/sortedstudentsapp_co/SortedStudentsApp_CO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortedstudentsapp_co;
+
+import java.util.*;
+
+/**
+ *
+ * @author Chidie17
+ */
+public class SortedStudentsApp_CO {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the Student Scores Application.\n");
+        System.out.println("Enter number of students to enter: ");
+        
+        
+    }
+    
+}
